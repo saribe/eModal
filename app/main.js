35d75be1b6b59d5b0d8ecf97fe659b2e04ef4cf0
Samuel Pinto <samuel@maispc.com>
2015-08-07 05:15:33 +0100

    Add titles

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {/* activate scrollspy menu */
 
     var hashTimer;
+    var iconPrefix = '.glyphicon-';
+    var t8 = window.toastr8;
 
     /* smooth scrolling sections */
     $('#navbar-collapsible li')
@@ -8,67 +10,74 @@ $(document).ready(function () {/* activate scrollspy menu */
         .find('a[href*=#]:not([href=#])')
             .click(hrefClick);
 
-    $('.glyphicon-cloud')       .click(ajaxDemo);
-    $('.glyphicon-comment')     .click(alertDemo);
-    $('.glyphicon-ok')          .click(confirmDemo);
-    $('.glyphicon-pencil')      .click(promptDemo);
-    $('.glyphicon-screenshot')  .click(iframeDemo);
+    $(iconPrefix + 'cloud')       .click(ajaxDemo);
+    $(iconPrefix + 'comment')     .click(alertDemo);
+    $(iconPrefix + 'ok')          .click(confirmDemo);
+    $(iconPrefix + 'pencil')      .click(promptDemo);
+    $(iconPrefix + 'screenshot')  .click(iframeDemo);
     ///////////////////* Implementation *///////////////////
 
-
+    // Demos
     function ajaxDemo() {
         var title = 'Ajax modal';
         var params = {
-            size: 'lg',
-            url: 'http://maispc.com/app/proxy.php?url=http://loripsum.net/api/' + Math.floor((Math.random() * 7) + 1) + '/short/ul/bq/prude/code/decorete',
             buttons: [
-                { text: 'Close', close: true, style: 'danger' },
-                { text: 'New contenet', close: false, style: 'success', click: ajaxDemo }
+               { text: 'Close', close: true, style: 'danger' },
+               { text: 'New contenet', close: false, style: 'success', click: ajaxDemo }
             ],
-            title: title
+            size: eModal.size.lg,
+            title: title,
+            url: 'http://maispc.com/app/proxy.php?url=http://loripsum.net/api/' + Math.floor((Math.random() * 7) + 1) + '/short/ul/bq/prude/code/decorete'
         };
 
         return eModal
             .ajax(params)
-            .then(function () { toastr8.info('Ajax Request complete!!!!', title) });
+            .then(function () { t8.info('Ajax Request complete!!!!', title) });
     }
 
     function alertDemo() {
-        return eModal.alert('You welcome! Want clean code ?', 'Alert modal');
+        var title = 'Alert modal';
+        return eModal
+            .alert('You welcome! Want clean code ?', title)
+            .then(function() { t8.facebook('Alert modal is visible.', title); });
     }
 
     function confirmDemo() {
         var title = 'Confirm modal callback feedback';
         return eModal
             .confirm('It is simple enough?', 'Confirm modal')
-            .then(function (/*DOM */) { toastr8.success('Thank you for your OK pressed!', title); })
-            .catch(function (/*null*/) { toastr8.error('Thank you for your Cancel pressed!', title) });
+            .then(function (/* DOM */) { t8.success('Thank you for your OK pressed!', title); })
+            .catch(function (/*null*/) { t8.skype('Thank you for your Cancel pressed!', title) });
     }
 
     function iframeDemo() {
+        var title = 'Insiders';
         return eModal
-            .iframe('https://www.youtube.com/embed/VTkvN51OPfI', 'Insiders')
-            .then(function () { toastr8.success('iFrame loaded!!!!', 'Load complete') });
+            .iframe('https://www.youtube.com/embed/VTkvN51OPfI', title)
+            .then(function () { t8.success('iFrame loaded!!!!', title) });
     }
 
     function promptDemo() {
         var title = 'Prompt modal callback feedback';
         return eModal
             .prompt({ size: eModal.size.sm, message: 'What\'s your name?', title: title })
-            .then(function (input) { toastr8.github({ message: 'Hi ' + input + '!', title: title, imgURI: 'https://avatars0.githubusercontent.com/u/4276775?v=3&s=89' }) })
-            .catch(function (input) { toastr8.android('Prompt dismissed by ' + input + '!', title); });
+            .then(function (input) { t8.github({ message: 'Hi ' + input + '!', title: title, imgURI: 'https://avatars0.githubusercontent.com/u/4276775?v=3&s=89' }) })
+            .catch(function (/**/) { t8.android('Why don\'t you tell me your name?', title); });
     }
 
+
+    //#region Page Events
     function hrefClick(e) {
         e.preventDefault();
         var hash = this.hash;
 
-        if (hash != location.hash) {
-            var scroll = $(this.hash).offset().top - 50 + $('#main').scrollTop();
+        if (hash !== location.hash) {
+            var query = '#main';
+            var scroll = $(this.hash).offset().top - 50 + $(query).scrollTop();
 
-            $('#main').stop().animate({
-                scrollTop: scroll
-            }, 1000);
+            $(query)
+                .stop()
+                .animate({ scrollTop: scroll }, 1000);
         }
     }
 
@@ -83,4 +92,5 @@ $(document).ready(function () {/* activate scrollspy menu */
             $el.prop('id', hash.slice(1));
         }, 400);
     }
+    //#endregion
 });
