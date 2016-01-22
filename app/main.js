8149f9b063b26d8074768bee5df058d362146093
Samuel Pinto <samuel@maispc.com>
2016-01-22 15:41:36 +0000

    Fix jQuery query with new version (2.2.0)
    The new jQuery version fails when not using quotes in attribute query
    
    e.g.:
    
    Before: 'a[href*=#]:not([href=#])'
    After:    'a[href*="#"]:not([href="#"])'

--- app/main.js
+++ app/main.js
@@ -1,97 +1,103 @@
-$(document).ready(function () {/* activate scroll spy menu */
+$(document)
+    .ready(function () {
+        var hashTimer;
+        var iconPrefix = '.glyphicon-';
+        var t8 = window.toastr8;
 
-    var hashTimer;
-    var iconPrefix = '.glyphicon-';
-    var t8 = window.toastr8;
+        /* smooth scrolling sections */
+        $('#navbar-collapsible li')
+            .on('activate.bs.scrollspy', _scrollspy)
+            .find('a[href*="#"]:not([href="#"])')
+                .click(_hrefClick);
 
-    /* smooth scrolling sections */
-    $('#navbar-collapsible li')
-        .on('activate.bs.scrollspy', scrollspy)
-        .find('a[href*=#]:not([href=#])')
-            .click(hrefClick);
+        $(iconPrefix + 'cloud').click(ajaxDemo);
+        $(iconPrefix + 'comment').click(alertDemo);
+        $(iconPrefix + 'ok').click(confirmDemo);
+        $(iconPrefix + 'pencil').click(promptDemo);
+        $(iconPrefix + 'screenshot').click(iframeDemo);
+        ///////////////////* Implementation *///////////////////
 
-    $(iconPrefix + 'cloud').click(ajaxDemo);
-    $(iconPrefix + 'comment').click(alertDemo);
-    $(iconPrefix + 'ok').click(confirmDemo);
-    $(iconPrefix + 'pencil').click(promptDemo);
-    $(iconPrefix + 'screenshot').click(iframeDemo);
-    ///////////////////* Implementation *///////////////////
+        // Demos
+        function ajaxDemo() {
+            var title = 'Ajax modal';
+            var params = {
+                buttons: [
+                   { text: 'Close', close: true, style: 'danger' },
+                   { text: 'New content', close: false, style: 'success', click: ajaxDemo }
+                ],
+                size: eModal.size.lg,
+                title: title,
+                url: 'http://maispc.com/app/proxy.php?url=http://loripsum.net/api/' + Math.floor((Math.random() * 7) + 1) + '/short/ul/bq/prude/code/decorete'
+            };
 
-    // Demos
-    function ajaxDemo() {
-        var title = 'Ajax modal';
-        var params = {
-            buttons: [
-               { text: 'Close', close: true, style: 'danger' },
-               { text: 'New content', close: false, style: 'success', click: ajaxDemo }
-            ],
-            size: eModal.size.lg,
-            title: title,
-            url: 'http://maispc.com/app/proxy.php?url=http://loripsum.net/api/' + Math.floor((Math.random() * 7) + 1) + '/short/ul/bq/prude/code/decorete'
-        };
-
-        return eModal
-            .ajax(params)
-            .then(function () { t8.info('Ajax Request complete!!!!', title) });
-    }
-
-    function alertDemo() {
-        var title = 'Alert modal';
-        return eModal
-            .alert('You welcome! Want clean code ?', title)
-            .then(function () { t8.facebook('Alert modal is visible.', title); });
-    }
-
-    function confirmDemo() {
-        var title = 'Confirm modal callback feedback';
-        return eModal
-            .confirm('It is simple enough?', 'Confirm modal')
-            .then(
-                function (/* DOM */) { t8.success('Thank you for your OK pressed!', title); },
-                function (/*null*/) { t8.skype('Thank you for your Cancel pressed!', title) });
-    }
-
-    function iframeDemo() {
-        var title = 'Insiders';
-        return eModal
-            .iframe('https://www.youtube.com/embed/VTkvN51OPfI', title)
-            .then(function () { t8.success('iFrame loaded!!!!', title) });
-    }
-
-    function promptDemo() {
-        var title = 'Prompt modal callback feedback';
-        return eModal
-            .prompt({ size: eModal.size.sm, message: 'What\'s your name?', title: title })
-            .then(
-                function (input) { t8.github({ message: 'Hi ' + input + '!', title: title, imgURI: 'https://avatars0.githubusercontent.com/u/4276775?v=3&s=89' }) },
-                function (/**/) { t8.android('Why don\'t you tell me your name?', title); });
-    }
-
-    //#region Page Events
-    function hrefClick(e) {
-        e.preventDefault();
-        var hash = this.hash;
-
-        if (hash !== location.hash) {
-            var query = '#main';
-            var scroll = $(this.hash).offset().top - 50 + $(query).scrollTop();
-
-            $(query)
-                .stop()
-                .animate({ scrollTop: scroll }, 1000);
+            return eModal
+                .ajax(params)
+                .then(function () { t8.info('Ajax Request complete!!!!', title) });
         }
-    }
 
-    function scrollspy() {
-        var el = this;
-        clearTimeout(hashTimer);
-        hashTimer = setTimeout(function () {
-            var hash = $(el).find('a').get(0).hash;
-            var $el = $(hash).prop('id', '');
+        function alertDemo() {
+            var title = 'Alert modal';
 
-            window.location.hash = hash;
-            $el.prop('id', hash.slice(1));
-        }, 400);
-    }
-    //#endregion
-});
+            return eModal
+                .alert('You welcome! Want clean code?', title)
+                .then(function () { t8.facebook('Alert modal is visible.', title); });
+        }
+
+        function confirmDemo() {
+            var title = 'Confirm modal callback feedback';
+
+            return eModal
+                .confirm('It is simple enough?', 'Confirm modal')
+                .then(
+                    function (/* DOM */) { t8.success('Thank you for your OK pressed!', title); },
+                    function (/*null*/) { t8.skype('Thank you for your Cancel pressed!', title) }
+                    );
+        }
+
+        function iframeDemo() {
+            var title = 'Insiders';
+
+            return eModal
+                .iframe('https://www.youtube.com/embed/VTkvN51OPfI', title)
+                .then(function () { t8.success('iFrame loaded!!!!', title) });
+        }
+
+        function promptDemo() {
+            var title = 'Prompt modal callback feedback';
+
+            return eModal
+                .prompt({ size: eModal.size.sm, message: 'What\'s your name?', title: title })
+                .then(
+                    function (input) { t8.github({ message: 'Hi ' + input + '!', title: title, imgURI: 'https://avatars0.githubusercontent.com/u/4276775?v=3&s=89' }) },
+                    function (/**/) { t8.android('Why don\'t you tell me your name?', title); });
+        }
+
+        //#region Page Events
+        function _hrefClick(e) {
+            e.preventDefault();
+            var hash = this.hash;
+
+            if (hash !== location.hash) {
+                var query = '#main';
+                var scroll = $(this.hash).offset().top - 50 + $(query).scrollTop();
+
+                $(query)
+                    .stop()
+                    .animate({ scrollTop: scroll }, 1000);
+            }
+        }
+
+        function _scrollspy() {
+            var el = this;
+            clearTimeout(hashTimer);
+
+            hashTimer = setTimeout(function () {
+                var hash = $(el).find('a').get(0).hash;
+                var $el = $(hash).prop('id', '');
+
+                window.location.hash = hash;
+                $el.prop('id', hash.slice(1));
+            }, 400);
+        }
+        //#endregion
+    });
