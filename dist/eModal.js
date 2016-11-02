ed9cf2a8cf7619abe487d20f4eaf68857f8e3c8d
Samuel Pinto <samuel@maispc.com>
2016-11-02 02:21:15 +0000

    add custom label

--- dist/eModal.js
+++ dist/eModal.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * SaRibe: eModal.js v1.2.6
+ * SaRibe: eModal.js v1.2.61
  * http://saribe.github.io/eModal
  * ========================================================================
  * Copyright Samuel Ribeiro.
@@ -22,37 +22,48 @@
  * <param name="title"    >The string header title or a flag to set default parameters.</param>
  * <returns type="Promise">{ then, element }.</returns>
  */
-; (function (define) {
+;
+(function (define) {
     define(['jquery'], function ($, root) {
         var $modal;
-        var bin = 'recycle-bin';
-        var div = '<div>';
-        var empty = '';
-        var eventClick = 'click';
-        var eventHide = 'hide';
-        var eventShown = 'shown.bs.modal';
-        var eventSubmit = 'submit';
-        var footerId = 'eFooter';
-        var hide = eventHide + '.bs.modal';
-        var input = 'input';
-        var keyDanger = 'danger';
-        var label = { OK: 'Cancel', True: 'False', Yes: 'No' };
+        var BIN = 'recycle-bin';
+        var DIV = '<div>';
+        var EMPTY = '';
+        var EVENT_CLICK = 'click';
+        var EVENT_HIDE = 'hide';
+        var EVENT_SHOW = 'shown.bs.modal';
+        var EVENT_SUBMIT = 'submit';
+        var FOOTER_ID = 'eFooter';
+        var HIDE = EVENT_HIDE + '.bs.modal';
+        var INPUT = 'input';
+        var KEY_DANGER = 'danger';
+        var LABEL = {
+            OK: 'Cancel',
+            True: 'False',
+            Yes: 'No'
+        };
         var lastParams = {};
-        var modalBody = 'modal-body';
+        var MODAL_BODY = 'modal-body';
+        var MODAL_DIALOG = '.modal-dialog';
         var options = {};
-        var recModalContent = 'modal-rec';
-        var size = { sm: 'sm', lg: 'lg', xl: 'xl' };
-        var tmpModalContent = 'modal-tmp';
+        var REC_MODAL_CONTENT = 'modal-rec';
+        var SIZE = {
+            sm: 'sm',
+            lg: 'lg',
+            xl: 'xl'
+        };
+        var TMP_MODAL_CONTENT = 'modal-tmp';
 
         var defaultSettings = {
             allowContentRecycle: true,
-            confirmLabel: Object.keys(label)[0],
-            labels: label,
+            confirmLabel: Object.keys(LABEL)[0],
+            labels: LABEL,
             loadingHtml: '<h5>Loading...</h5><div class=progress><div class="progress-bar progress-bar-striped active" style="width: 100%"></div></div>',
-            size: empty,
+            size: EMPTY,
             title: 'Attention'
         };
 
+        root.addLabel = addLabel;
         root.ajax = ajax;
         root.alert = alert;
         root.close = close;
@@ -62,8 +73,8 @@
         root.prompt = prompt;
         root.setEModalOptions = setEModalOptions;
         root.setModalOptions = setModalOptions;
-        root.size = size;
-        root.version = '1.2.6';
+        root.size = SIZE;
+        root.version = '1.2.61';
 
         return root;
 
@@ -76,7 +87,7 @@
             $modal
                 .modal(options)
                 .find('.modal-content')
-                    .append(message);
+                .append(message);
         }
 
         /**
@@ -92,17 +103,21 @@
 
             var q;
 
-            try { q = require('Q'); }   // Load option Q by require if exist
-            catch (e) { q = window.Q; }
+            try {
+                q = require('Q');
+            } // Load option Q by require if exist
+            catch (e) {
+                q = window.Q;
+            }
 
-            if (q) {                    // try Q
+            if (q) { // try Q
                 defer = q.defer();
-            } else {                    // Use jQuery :(
+            } else { // Use jQuery :(
                 defer = $.Deferred();
                 defer.promise = defer.promise();
             }
 
-            defer.promise.element = $modal;
+            defer.promise.element = $modal.find(MODAL_DIALOG);
             return defer;
         }
 
@@ -112,9 +127,11 @@
          * @returns {$DOM} footer DOM element
          */
         function _getFooter(buttons) {
-            if (buttons === false) { return empty; }
+            if (buttons === false) {
+                return EMPTY;
+            }
 
-            var messageFotter = $(div).addClass('modal-footer').prop('id', footerId);
+            var messageFotter = $(DIV).addClass('modal-footer').prop('id', FOOTER_ID);
             if (buttons) {
                 for (var i = 0, k = buttons.length; i < k; i++) {
                     var btnOp = buttons[i];
@@ -127,10 +144,10 @@
                                     //add close event
                                     if (btnOp[index]) {
                                         btn.attr('data-dismiss', 'modal')
-                                           .addClass('x');
+                                            .addClass('x');
                                     }
                                     break;
-                                case eventClick:
+                                case EVENT_CLICK:
                                     //click event
                                     var fn = btnOp.click.bind(_getModalInstance(true).find('.modal-content'));
                                     btn.click(fn);
@@ -170,11 +187,11 @@
                     $(content).clone() :
                     $(content);
 
-                $message.addClass(data.useBin && !data.loading ? recModalContent : tmpModalContent);
+                $message.addClass(data.useBin && !data.loading ? REC_MODAL_CONTENT : TMP_MODAL_CONTENT);
             } else {
-                $message = $(div)
+                $message = $(DIV)
                     .attr('style', 'position:relative;word-wrap:break-word;')
-                    .addClass(modalBody)
+                    .addClass(MODAL_BODY)
                     .html(content);
             }
 
@@ -189,8 +206,8 @@
         function _getModalInstance(skipEventChageIfExists) {
             if (!$modal) {
                 //add recycle bin container to document
-                if (!$('#' + bin).length) {
-                    $('body').append($(div).prop('id', bin).hide());
+                if (!$('#' + BIN).length) {
+                    $('body').append($(DIV).prop('id', BIN).hide());
                 }
 
                 $modal = createModalElement();
@@ -201,34 +218,34 @@
             }
 
             return $modal
-                .on(eventShown, function () {
-                    $(this).find(input).first().focus();
+                .on(EVENT_SHOW, function () {
+                    $(this).find(INPUT).first().focus();
                 });
 
             function createModalElement() {
-                return $('<div class="modal fade" tabindex="-1"><style>.modal-xl{width:96%;}</style>'
-                + '<div class=modal-dialog>'
-                + '<div class=modal-content>'
-                + ' <div class=modal-header><button type=button class="x close" data-dismiss=modal><span aria-hidden=true>&times;</span><span class=sr-only>Close</span></button><h4 class=modal-title></h4></div>'
-                + '</div>'
-                + '</div>'
-                + '</div>')
-                .on('hidden.bs.modal', _recycleModal)
-                .on(eventClick, 'button.x', function (ev) {
-                    var btn = $(ev.currentTarget);
+                return $('<div class="modal fade" tabindex="-1"><style>.modal-xl{width:96%;}</style>' +
+                        '<div class=modal-dialog>' +
+                        '<div class=modal-content>' +
+                        ' <div class=modal-header><button type=button class="x close" data-dismiss=modal><span aria-hidden=true>&times;</span><span class=sr-only>Close</span></button><h4 class=modal-title></h4></div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>')
+                    .on('hidden.bs.modal', _recycleModal)
+                    .on(EVENT_CLICK, 'button.x', function (ev) {
+                        var btn = $(ev.currentTarget);
 
-                    if (btn.prop('type') !== eventSubmit)
-                        return $modal.modal(eventHide);
+                        if (btn.prop('type') !== EVENT_SUBMIT)
+                            return $modal.modal(EVENT_HIDE);
 
-                    try {
-                        if (btn.closest('form')[0].checkValidity())
+                        try {
+                            if (btn.closest('form')[0].checkValidity())
+                                return close();
+                        } catch (e) {
                             return close();
-                    } catch (e) {
-                        return close();
-                    }
+                        }
 
-                    return $modal;
-                });
+                        return $modal;
+                    });
             }
         }
 
@@ -240,12 +257,12 @@
         function _recycleModal() {
             if (!$modal) return;
 
-            var $content = $modal.find('.' + recModalContent).removeClass(recModalContent)
-                   .appendTo('#' + bin);
+            var $content = $modal.find('.' + REC_MODAL_CONTENT).removeClass(REC_MODAL_CONTENT)
+                .appendTo('#' + BIN);
 
             $modal
-                .off(hide)
-                .off(eventShown)
+                .off(HIDE)
+                .off(EVENT_SHOW)
                 .find('.modal-content > div:not(:first-child)')
                 .remove();
 
@@ -271,7 +288,7 @@
             var $ref = _getModalInstance();
 
             //#region change size
-            $ref.find('.modal-dialog')
+            $ref.find(MODAL_DIALOG)
                 .removeClass('modal-sm modal-lg modal-xl')
                 .addClass(params.size ? 'modal-' + params.size : defaultSettings.size);
             //#endregion
@@ -279,14 +296,19 @@
             //#region change title
             $ref.find('.modal-title')
                 .html((params.title || title || defaultSettings.title) + ' ')
-                .append($('<small>').html(params.subtitle || empty));
+                .append($('<small>').html(params.subtitle || EMPTY));
             //#endregion
 
-            $ref.on(hide, params.onHide);
+            $ref.on(HIDE, params.onHide);
         }
         //#endregion
 
         //#region /////////////////////////* Public Methods */////////////////////////
+
+        function addLabel(yes, no) {
+            LABEL[yes] = no;
+        }
+
         /**
          * Gets data from URL to eModal body
          * @param {Object | String} data - this can be the message string or the full detailed object
@@ -305,7 +327,9 @@
                 dataType: 'text'
             };
 
-            if (data.url) { $.extend(params, data); }
+            if (data.url) {
+                $.extend(params, data);
+            }
 
             $.ajax(params)
                 .success(ok)
@@ -315,19 +339,20 @@
 
             function ok(html) {
                 $modal
-                    .find('.' + modalBody)
+                    .find('.' + MODAL_BODY)
                     .html(html);
 
                 return dfd.resolve($modal);
             }
-            function error(responseText/*, textStatus*/) {
+
+            function error(responseText /*, textStatus*/ ) {
                 var msg = '<div class="alert alert-danger">' +
                     '<strong>XHR Fail: </strong>URL [ ' + params.url + '] load fail.' +
                     '</div>';
 
                 $modal
-                   .find('.' + modalBody)
-                   .html(msg);
+                    .find('.' + MODAL_BODY)
+                    .html(msg);
                 return dfd.reject(responseText);
             }
         }
@@ -342,11 +367,13 @@
             _setup(data, title);
 
             var dfd = data.deferred || _createDeferred();
-            var $message = $(div).append(_getMessage(data), _getFooter(data.buttons));
+            var $message = $(DIV).append(_getMessage(data), _getFooter(data.buttons));
 
             _build($message);
 
-            if (!data.async) { $modal.on(eventShown, dfd.resolve); }
+            if (!data.async) {
+                $modal.on(EVENT_SHOW, dfd.resolve);
+            }
 
             return dfd.promise;
         }
@@ -362,10 +389,16 @@
 
             return alert({
                 async: true,
-                buttons: [
-                    { close: true, click: click, text: label[data.label] ? label[data.label] : label[defaultSettings.confirmLabel], style: keyDanger },
-                    { close: true, click: click, text: label[data.label] ? data.label : defaultSettings.confirmLabel }
-                ],
+                buttons: [{
+                    close: true,
+                    click: click,
+                    text: LABEL[data.label] ? LABEL[data.label] : LABEL[defaultSettings.confirmLabel],
+                    style: KEY_DANGER
+                }, {
+                    close: true,
+                    click: click,
+                    text: LABEL[data.label] ? data.label : defaultSettings.confirmLabel
+                }],
                 deferred: dfd,
                 message: data.message || data,
                 onHide: click,
@@ -377,7 +410,7 @@
                 close();
 
                 var key = $(ev.currentTarget).html();
-                return label[key] ? dfd.resolve() : dfd.reject();
+                return LABEL[key] ? dfd.resolve() : dfd.reject();
             }
         }
 
@@ -390,7 +423,7 @@
         function iframe(params, title) {
             var dfd = _createDeferred();
             var html = ('<div class=modal-body style="position: absolute;width: 100%;background-color: rgba(255,255,255,0.8);height: 100%;">%1%</div>' +
-                        '<iframe class="embed-responsive-item" frameborder=0 src="%0%" style="width:100%;height:75vh;display:block;"/>')
+                    '<iframe class="embed-responsive-item" frameborder=0 src="%0%" style="width:100%;height:75vh;display:block;"/>')
                 .replace('%0%', params.message || params.url || params)
                 .replace('%1%', defaultSettings.loadingHtml);
 
@@ -402,7 +435,7 @@
                 buttons: params.buttons || false,
                 deferred: dfd,
                 message: message,
-                size: params.size || size.xl,
+                size: params.size || SIZE.xl,
                 title: params.title || title
             });
             //////////
@@ -410,7 +443,7 @@
             function iframeReady() {
                 $(this)
                     .parent()
-                    .find('div.' + tmpModalContent)
+                    .find('div.' + TMP_MODAL_CONTENT)
                     .fadeOut(function () {
                         $(this).remove();
                     });
@@ -424,7 +457,7 @@
          * @returns {Array} All removed elements
          */
         function emptyBin() {
-            return $('#' + bin + ' > *').remove();
+            return $('#' + BIN + ' > *').remove();
         }
 
         /**
@@ -441,8 +474,7 @@
 
             if (typeof data === 'object') {
                 $.extend(params, data);
-            }
-            else {
+            } else {
                 params.message = data;
                 params.title = title;
             }
@@ -458,30 +490,36 @@
                 }
             }
 
-            var buttons = _getFooter([
-                { close: true, type: 'reset', text: label.OK, style: keyDanger },
-                { close: false, type: eventSubmit, text: defaultSettings.confirmLabel }
-            ].concat(params.buttons || []));
+            var buttons = _getFooter([{
+                close: true,
+                type: 'reset',
+                text: LABEL.OK,
+                style: KEY_DANGER
+            }, {
+                close: false,
+                type: EVENT_SUBMIT,
+                text: defaultSettings.confirmLabel
+            }].concat(params.buttons || []));
 
             params.buttons = false;
             params.onHide = submit;
 
             params.message = $('<form role=form style="margin-bottom:0;">' +
                     '<div class=modal-body>' +
-                    '<label for=prompt-input class=control-label>' + (params.message || empty) + '</label>' +
-                    '<input type=text class=form-control required autocomplete="on" value="' + (params.value || empty) + (params.pattern ? '" pattern="' + params.pattern : empty) + '">' +
+                    '<label for=prompt-input class=control-label>' + (params.message || EMPTY) + '</label>' +
+                    '<input type=text class=form-control required autocomplete="on" value="' + (params.value || EMPTY) + (params.pattern ? '" pattern="' + params.pattern : EMPTY) + '">' +
                     '</div></form>')
                 .append(buttons)
-                .on(eventSubmit, submit);
+                .on(EVENT_SUBMIT, submit);
 
             return alert(params);
 
             function submit(ev) {
-                var value = $modal.find(input).val();
+                var value = $modal.find(INPUT).val();
                 close();
 
                 //TODO:
-                ev.type !== eventSubmit ?
+                ev.type !== EVENT_SUBMIT ?
                     dfd.reject(value) :
                     dfd.resolve(value);
 
@@ -515,7 +553,7 @@
          */
         function close() {
             if ($modal) {
-                $modal.off(hide).modal(eventHide);
+                $modal.off(HIDE).modal(EVENT_HIDE);
             }
             return $modal;
         }
