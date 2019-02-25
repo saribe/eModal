91581c414b16c38cb0a72d4ca0c10d02bc3518a1
yuceldaglar <25679943+yuceldaglar@users.noreply.github.com>
2019-02-25 19:03:17 +0300

    bootstrap 4 title bug fixed

--- dist/eModal.js
+++ dist/eModal.js
@@ -235,7 +235,7 @@
                 return $('<div class="modal fade" tabindex="-1"><style>.modal-xl{width:96%;}.modal-body{max-height: calc(100vh - 145px);overflow-y: auto;}</style>' +
                     '<div class=modal-dialog>' +
                     '<div class=modal-content>' +
-                    ' <div class=modal-header><button type=button class="x close" data-dismiss=modal aria-label="Close"><span aria-hidden=true>&times;</span></button><h5 class=modal-title></h5></div>' +
+                    ' <div class=modal-header><h5 class=modal-title></h5><button type=button class="x close" data-dismiss=modal aria-label="Close"><span aria-hidden=true>&times;</span></button></div>' +
                     '</div>' +
                     '</div>' +
                     '</div>')
